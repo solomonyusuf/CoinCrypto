3c23dd7ac7721866f1a83b1a05fd392f7578d84f
Solomon yusuf <olafire03@gmail.com>
2025-03-10 19:09:08 +0100

    fetch users on admin

--- app/Http/Livewire/Admin/UsersComponent.php
+++ app/Http/Livewire/Admin/UsersComponent.php
@@ -2,12 +2,61 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\User;
 use Livewire\Component;
 
 class UsersComponent extends Component
 {
+    public $users;
+    public $model = [
+        'role_id',
+		'image',
+		'info',
+		'linkedin',
+		'twitter',
+		'first_name',
+		'last_name',
+		'email',
+		'email_verified_at',
+		'password',
+    ];
+
+    public function GetAll()
+    {
+        return User::orderByDesc('created_at')->get();
+    }
+    public function create()
+    {
+        User::create($this->model);
+
+        toast('Creation Successful', 'success');
+
+        $this->model = [];
+
+        $this->users = $this->GetAll();
+    }
+     public function update($id)
+    {
+        User::find($id)->update($this->model);
+
+        toast('Update Successful', 'success');
+        
+        $this->model = [];
+
+        $this->users = $this->GetAll();
+    }
+     public function delete($id)
+    {
+        User::find($id)->delete();
+
+        toast('Deletion Successful', 'success');
+
+        $this->users = $this->GetAll();
+    }
     public function render()
     {
+        $this->users = $this->GetAll();
+
         return view('livewire.admin.users-component')
         ->layout('layouts.admin');
     }

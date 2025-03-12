ff13607cda554f63d0e498b257fb330bef7fa572
Solomon yusuf <olafire03@gmail.com>
2025-03-12 09:50:06 +0100

    correctio

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -154,7 +154,7 @@
             <img src="{{ asset('/storage/'.$data->image) }}" class="card-img-top" alt="modernize-img">
             </a>
             
-            <img src="{{ $data?->article_creators->where(['originator'=> true])->first()?->user?->image ?? $latest?->article_creators->first()?->user?->image }}" alt="modernize-img" class="img-fluid rounded-circle position-absolute bottom-0 start-0 mb-n9 ms-9" width="40" height="40" data-bs-toggle="tooltip" data-bs-placement="top" >
+            <img src="{{ $data?->article_creators->where(['originator'=> true])->first()?->user?->image ?? $data?->article_creators->first()?->user?->image }}" alt="modernize-img" class="img-fluid rounded-circle position-absolute bottom-0 start-0 mb-n9 ms-9" width="40" height="40" data-bs-toggle="tooltip" data-bs-placement="top" >
           </div>
           <div class="card-body p-4">
             <span class="badge text-bg-light fs-2 py-1 px-2 lh-sm  mt-3">

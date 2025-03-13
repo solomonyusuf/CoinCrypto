b92c31d2722ec1f6703cf7363d9f16339ec78a74
Solomon yusuf <olafire03@gmail.com>
2025-03-13 16:45:54 +0100

    fixed upload issue

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -6,24 +6,24 @@ use Illuminate\Http\Request;
 
 class UploadController
 {
-    public static function UploadFile(Request $request, $type = 'image')
+    public static function UploadFile(Request $request)
     {
         $client = $request?->image->getClientOriginalExtension();
 
         if ($client == 'pdf' || $client == 'doc' || $client == 'jpg' || $client == 'png' || $client == 'jpeg') {
-            $file = asset('uploads/'. uniqid() . time() . '.' . request()->image->getClientOriginalName());
-            request()->image->move(public_path('uploads'), $file);
+            $file = asset('uploads/'. uniqid() . time() . '.' . $request->image->getClientOriginalName());
+            $request->image->move(public_path('uploads'), $file);
 
             return $file;
         }
     }
-    public static function UploadFile2(Request $request, $type = '')
+    public static function UploadFile2(Request $request)
     {
         $client = $request?->host_image->getClientOriginalExtension();
 
             if ($client == 'pdf' || $client == 'doc' || $client == 'jpg' || $client == 'png' || $client == 'jpeg') {
                 $file = asset('uploads/'. uniqid() . time() . '.' . request()->host_image->getClientOriginalName());
-                request()->host_image->move(public_path('uploads'), $file);
+                $request->host_image->move(public_path('uploads'), $file);
 
                 return $file;
             }

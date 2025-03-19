48107a65c48e71ac44cf0b9904d8f3a0f17bb663
Solomon yusuf <olafire03@gmail.com>
2025-03-19 07:48:46 +0100

    added events image

--- resources/views/livewire/page-event-component.blade.php
+++ resources/views/livewire/page-event-component.blade.php
@@ -19,24 +19,32 @@
         <div class="bg-primary rounded">
           <div class="d-flex relative shadow-md container-fluid justify-center items-center">
             <div class="row d-flex justify-content-between justify-content-center items-center pt-1 pb-1 w-100">
-              <div class="col-md-5 col-sm-12 justify-content-center pb-4">
-                <h3 class="text-white fw-semibold mt-4 mb-3">
-                  {{ $event->title }}
-                </h3>
-                <span class="text-white mt-2 mb-1 ">
-                  {!! $event->content !!}
-                </span>
-                <a href="{{ $event->category }}" class="register-btn ms-3">Register Now</a>
-                  
+              <div class="col-md-5 col-sm-12 d-flex gap-2 justify-content-center pb-4">
+                <div>
+                  <img src="{{ $event?->image }}" alt="user" width="120" height="120" class="rounded-circle ms-3">
+                </div>
+              <div>
+              <h3 class="text-white fw-semibold mt-4 mb-3">
+                {{ $event->title }}
+              </h3>
+              <span class="text-white mt-2 mb-1 ">
+                {!! $event->content !!}
+              </span>
+              <a href="{{ $event->category }}" class="register-btn ms-3">Register Now</a>
+                
+             </div>
+               
               </div>
               <div class="col-md-7 col-sm-12 justify-content-center ">
-                <div class="countdown-container gap-2">
-                  <div class="d-flex gap-2">
-                      <div class="countdown-box">
-                          <span id="days">{{ \Carbon\Carbon::parse($event->created_at)->day}}</span>
-                          <span class="countdown-label">{{ \Carbon\Carbon::parse($event->created_at)->format('F')}}</span>
-                      </div>
-                  </div>
+                <div class="countdown-container">
+                  <div class="row align-items-center">
+                    <div class="countdown-box">
+                        <span id="days">{{ \Carbon\Carbon::parse($event->created_at)->day }}</span>
+                        <span class="countdown-label">{{ \Carbon\Carbon::parse($event->created_at)->format('F') }}</span>
+                    </div>
+                    
+                </div>
+                
                   
               </div>
               </div>

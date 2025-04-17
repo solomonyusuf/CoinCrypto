44fbe04e288afd4cee46abdb11733eb53102dc00
Solomon yusuf <olafire03@gmail.com>
2025-04-17 13:16:51 +0100

    fix auth

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,7 +6,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>{{ $setting->name }} Auth</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset($setting->logo) }}" />
+
     <script src="https://cdn.tailwindcss.com"></script>
     @livewireStyles
     <link rel="stylesheet" href="{{ asset('css/fonts.css') }}" />

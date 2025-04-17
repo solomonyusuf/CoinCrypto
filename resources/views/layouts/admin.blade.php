f9099942bb4104b4014280a1cc93aa1460f51c01
Solomon yusuf <olafire03@gmail.com>
2025-04-17 12:53:29 +0100

    fix admin-dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -59,7 +59,7 @@
             <!-- ---------------------------------- -->
             <div class="brand-logo d-flex align-items-center justify-content-between">
               <a href="{{ route('dashboard') }}" class="text-nowrap logo-img">
-                <img src="{{ asset($setting->logo) }}" style="height:40px;" class="dark-logo" alt="Logo-Dark">
+                <img src="{{ asset($setting->logo) }}" style="height:100px;width:150px;" class="dark-logo" alt="Logo-Dark">
               </a>
               <a href="javascript:void(0)" class="sidebartoggler ms-auto text-decoration-none fs-5 d-block d-xl-none">
                 <i class="ti ti-x"></i>
--- resources/views/livewire/admin/dashboard-component.blade.php
+++ resources/views/livewire/admin/dashboard-component.blade.php
@@ -4,7 +4,7 @@
 <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
-              <div class="card border-0 zoom-in bg-primary-subtle shadow-none">
+              <div class="card border-0 zoom-in bg-info-subtle shadow-none">
                 <div class="card-body">
                   <div class="text-center">
                     <img src="assets/images/svgs/icon-user-male.svg" width="50" height="50" class="mb-3" >

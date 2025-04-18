0e37becb2c07172f19a9553c659c2ffae72bbbc7
Solomon yusuf <olafire03@gmail.com>
2025-04-18 09:49:39 +0100

    add image2 upload

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1542,7 +1542,7 @@
          {
               CKEDITOR.replace(textarea.id,{
                 allowedContent: true,
-                extraPlugins: 'codesnippet',
+                extraPlugins: 'codesnippet,image2',
                 codeSnippet_theme: 'monokai_sublime',
                     filebrowserUploadurl: "{{route('upload_image', ['_token' => csrf_token() ])}}",
                     filebrowserUploadMethod: 'form'
